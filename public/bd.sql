e837175ebe580cd81a33d31c67f8c237832abeb5
contrerasalexa <akikoalexamma80@gmail.com>
2025-05-16 11:48:02 -0600

    Remove unused SQL seed file and clean up code comments in app.js

--- public/bd.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO alimento (nombre, imagen, energia_kcal, proteina, carbohidrato, grasa)
-VALUES 
-  ('Aceite', '/public/alimentos/aceite.png', 90, 0, 0, 10),
-  ('Aguacate', '/public/alimentos/aguacate.png', 80, 1, 2, 7),
-  ('Arroz', '/public/alimentos/arroz.png', 130, 2, 28, 1),
-  ('Atún', '/public/alimentos/atun.png', 99, 20, 0, 1),
-  -- sigue con los demás...
-  ('Pechuga de pollo', '/public/alimentos/pechuga-pollo.png', 120, 26, 0, 2);
